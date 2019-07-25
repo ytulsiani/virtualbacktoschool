a90b4a907b47b67a65bddd155e93d9bce98169ea
Cindy Dovinh <cindy.dovinh@gmail.com>
2019-07-25 14:35:51 -0700

    Add files via upload

--- js/itemList.js
+++ js/itemList.js
@@ -1,41 +1,51 @@
 var itemList = [
   {
     "quantity": 0,
+    "image": "img/backtoschool/kidsock.png",
+    "itemName": "Kid<br />Socks",
+    "servingUnits": "6-Pack",
+    "ourPrice": 8,
+    "retailPrice": 120,
+    "unitsReceipt": "pack",
+    "nameReceipt": "Socks"
+  },
+{
+    "quantity": 0,
+    "image": "img/backtoschool/teensocks.png",
+    "itemName": "Teen<br />Socks",
+    "servingUnits": "6-Pack",
+    "ourPrice": 10,
+    "retailPrice": 21,
+    "unitsReceipt": "packs",
+    "nameReceipt": "Socks"
+  },
+{
+    "quantity": 0,
+    "image": "img/backtoschool/kidsupplies.png",
+    "itemName": "School<br />Supplies",
+    "servingUnits": "set",
+    "ourPrice": 15,
+    "retailPrice": 30,
+    "unitsReceipt": "set",
+    "nameReceipt": "Supplies"
+  },
+{
+ "quantity": 0,
     "image": "img/backtoschool/kidpack.png",
-    "itemName": "Kid Backpack",
+    "itemName": "Kid <br />Backpack",
     "servingUnits": "each",
     "ourPrice": 20,
     "retailPrice": 60,
     "unitsReceipt": "unit",
     "nameReceipt": "Backpack"
   },
-  {
-    "quantity": 0,
-    "image": "img/backtoschool/kidsupplies.png",
-    "itemName": "School Supplies",
-    "servingUnits": "set",
-    "ourPrice": 10,
-    "retailPrice": 30,
-    "unitsReceipt": "set",
-    "nameReceipt": "Supplies"
-  },
-    {
-    "quantity": 0,
-    "image": "img/backtoschool/kidsneak.png",
-    "itemName": "Kid Sneakers",
-    "servingUnits": "pair",
-    "ourPrice": 30,
-    "retailPrice": 30,
-    "unitsReceipt": "pair",
-    "nameReceipt": "Sneakers"
-  },
-
+  
   {
     "quantity": 0,
     "image": "img/backtoschool/kidcoat.png",
-    "itemName": "Kid Coat",
+    "itemName": "Kid<br />Coat",
     "servingUnits": "each",
-    "ourPrice": 20,
+    "ourPrice": 25,
     "retailPrice": 35,
     "unitsReceipt": "unit",
     "nameReceipt": "Coat"
@@ -43,85 +53,77 @@ var itemList = [
   {
     "quantity": 0,
     "image": "img/backtoschool/kidboots.png",
-    "itemName": "Kid Boots",
+    "itemName": "Kid<br />Boots",
     "servingUnits": "pair",
-    "ourPrice": 25,
+    "ourPrice": 30,
     "retailPrice": 85,
     "unitsReceipt": "pair",
     "nameReceipt": "Boots"
   },
-
-  {
+    {
     "quantity": 0,
-    "image": "img/backtoschool/kidsock.png",
-    "itemName": "Kid Socks",
-    "servingUnits": "6-Pack",
-    "ourPrice": 8,
-    "retailPrice": 120,
-    "unitsReceipt": "pack",
-    "nameReceipt": "Socks"
+    "image": "img/backtoschool/kidsneak.png",
+    "itemName": "Kid<br />Sneakers",
+    "servingUnits": "pair",
+    "ourPrice": 35,
+    "retailPrice": 30,
+    "unitsReceipt": "pair",
+    "nameReceipt": "Sneakers"
   },
   {
     "quantity": 0,
     "image": "img/backtoschool/teenpack.png",
-    "itemName": "Teen Backpack",
+    "itemName": "Teen<br />Backpack",
     "servingUnits": "each",
-    "ourPrice": 20,
+    "ourPrice": 35,
     "retailPrice": 30,
     "unitsReceipt": "unit",
     "nameReceipt": "Backpack"
   },
+
   {
     "quantity": 0,
     "image": "img/backtoschool/teencoat.png",
-    "itemName": "Teen Coat",
+    "itemName": "Teen<br />Coat",
     "servingUnits": "each",
-    "ourPrice": 50,
+    "ourPrice": 40,
     "retailPrice": 75,
     "unitsReceipt": "unit",
     "nameReceipt": "Coat"
-  },
-   {
-    "quantity": 0,
-    "image": "img/backtoschool/teensneak2.png",
-    "itemName": "Sneakers",
-    "servingUnits": "pair",
-    "ourPrice": 30,
-    "retailPrice": 99,
-    "unitsReceipt": "pair",
-    "nameReceipt": "Sneakers"
   },
 
 
   {
     "quantity": 0,
     "image": "img/backtoschool/teenboot.png",
-    "itemName": "Teen Boots",
+    "itemName": "Teen<br />Boots",
     "servingUnits": "20lbs bag",
-    "ourPrice": 40,
+    "ourPrice": 45,
     "retailPrice": 45,
     "unitsReceipt": "pair",
     "nameReceipt": "Boots"
   },
+   {
+    "quantity": 0,
+    "image": "img/backtoschool/teensneak2.png",
+    "itemName": "Teen<br />Sneakers",
+    "servingUnits": "pair",
+    "ourPrice": 50,
+    "retailPrice": 99,
+    "unitsReceipt": "pair",
+    "nameReceipt": "Sneakers"
+  },
+
   {
     "quantity": 0,
     "image": "img/backtoschool/teensneak.png",
-    "itemName": "Athletic Shoes",
+    "itemName": "Athletic<br />Shoes",
     "servingUnits": "pair",
-    "ourPrice": 45,
+    "ourPrice": 55,
     "retailPrice": 15,
     "unitsReceipt": "pair",
     "nameReceipt": "Sneakers"
   },
 
-   {
-    "quantity": 0,
-    "image": "img/backtoschool/teensocks.png",
-    "itemName": "Teen Socks",
-    "servingUnits": "6-Pack",
-    "ourPrice": 10,
-    "retailPrice": 21,
-    "unitsReceipt": "packs",
-    "nameReceipt": "Socks"
-  }
+   
 ]
